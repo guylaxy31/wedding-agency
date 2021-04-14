92c1d557feac28975b4847610dbc01cac40f0712
Guylaxy <kritsana.mnp@gmail.com>
2021-04-14 14:58:05 +0700

    เพิ่ม Header + Responsive

---
+++ src/components/WeddingAgencyLogo.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const WeddingAgencyLogo = ({ className }) => {
+    return (
+        <svg className={className} xmlns="http://www.w3.org/2000/svg" width="171" height="41" viewBox="0 0 171 41">
+            <text id="Wedding_Agency" data-name="Wedding Agency" transform="translate(0 23)" fill="#fff" font-size="30" font-family="ZapfinoExtraLT-SmallCaps, Zapfino Extra LT Small Caps"><tspan x="0" y="0">Wedding Agency</tspan></text>
+        </svg>
+
+    )
+}
+
+export default WeddingAgencyLogo;
---
+++ src/screens/Header.js
@@ -0,0 +1,39 @@
+import './Header.scss'
+import WeddingAgencyLogo from '../components/WeddingAgencyLogo';
+import React, { useState, useEffect } from 'react';
+
+export default function Header() {
+    const [drawerstate, setdrawerstate] = useState(false);
+    const [drawerstyle, setdrawerstyle] = useState('none');
+    const [width, setWidth] = useState(window.innerWidth);
+    const handleResize = () => setWidth(window.innerWidth);
+    const toggleDrawer = () => {
+        setdrawerstate(!drawerstate)
+        drawerstate === true ? setdrawerstyle('flex') : setdrawerstyle('none');
+
+    }
+
+    useEffect(() => {
+        window.addEventListener("resize", handleResize);
+    }, [width])
+
+    return (
+        <div className="header">
+            <div className="header-bg">
+                <div className="drawer-btn"><img onClick={() => toggleDrawer()} src="./public-icon/drawer-icon.png"></img></div>
+                <div className="nav-container">
+                    <div className="wedding-logo"><WeddingAgencyLogo className="main-logo" /></div>
+                    {width > 768 ? <div className="nav-menu-list" ><a href="#">Home</a><a href="#">About us</a><a href="#">Services</a><a href="#">Gallery</a><a href="#">Blog</a><a href="#">Contact</a></div>
+                        : <div className="nav-menu-list" style={{ display: drawerstyle }}><a href="#">Home</a><a href="#">About us</a><a href="#">Services</a><a href="#">Gallery</a><a href="#">Blog</a><a href="#">Contact</a></div>
+                    }
+                    <div></div>
+                </div>
+                <div className="header-content">
+                    <h1>We Are Best Wedding Agency</h1>
+                    <p>Perfect day & perfect time</p>
+                    <div className="view-more-btn"><p>View More</p></div>
+                </div>
+            </div>
+        </div>
+    )
+}
