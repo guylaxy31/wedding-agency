9011aed38a51b4bde6da1fbe49740efe7d2c6160
Guylaxy <kritsana.mnp@gmail.com>
2021-04-14 20:22:13 +0700

    whatever + Responsive

---
+++ src/components/Whatever.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Whatever.scss'
+
+function Whatever() {
+    return (
+        <div className="whatever">
+            <div className="whatever-bg">
+                <div className="whatever-container"><p className="whatever-p1">Whatever our souls are made of, his and mine are the same.</p>
+                    <p className="whatever-p2">- Emily Brontë</p></div>
+            </div>
+        </div>
+    )
+}
+
+export default Whatever
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -2,7 +2,7 @@ import Header from '../components/Header'
 import Services from '../components/Services'
 import Invitation from '../components/Invitation'
 import Gallery from '../components/Gallery'
-
+import Whatever from '../components/Whatever'
 import './Home.scss'
 
 function Home() {
@@ -12,6 +12,7 @@ function Home() {
             <Services />
             <Invitation />
             <Gallery />
+            <Whatever />
         </div>
     );
 }
